c33d58eef5a927a0d395f4676d3018617ad940b7
karbuc <karsten.buckstegge@sinnerschrader.com>
2015-11-02 18:12:57 +0100

    refactor route

--- source/application/routes/index.js
+++ source/application/routes/index.js
@@ -1,7 +1,7 @@
 import readTree from '../../library/read-tree';
 
 const promiseReadTree = (root) => {
-	return new Promise(function(resolve, reject) {
+	return new Promise((resolve, reject) => {
 		readTree(root, (err, res) => {
 			if(err) {
 				return reject(err);
@@ -17,24 +17,10 @@ export default function bookRouteFactory (application) {
 		try {
 			const results = await promiseReadTree('./static/images/');
 			this.body = results;
+			return results;
 		} catch (err) {
 			console.log('woot');
 			this.body = 'woot';
 		}
-		/*try {
-			let result;
-			readTree('./static/images/', (err, res) => {
-				result = 'Nothing here!';
-				if (res) {
-					this.result = JSON.stringify(res[0]);
-				}
-				console.log('result: ' + this.result);
-			});
-
-		}
-		catch(err) {
-			console.log(err);
-		} */
 	}
-
 }
